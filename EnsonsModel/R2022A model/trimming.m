65d0c3ba4970b713c1c845e1b540b519ce7aea21
YanxingS <yanxings@usc.edu>
2024-03-24 15:30:12 -0700

    updated model for hw

--- EnsonsModel/R2022A model/trimming.m
+++ EnsonsModel/R2022A model/trimming.m
@@ -1,10 +1,21 @@
-% trimming model
-clc; clear;
+clc; clear; close all
 
-ic = [0 0 0 13 0 0 0 0 0];
+% trimming aircraft model
 
-states = ['phi' 'theta' 'psi' 'Vx' 'Vy' 'Vz' 'p' 'q' 'r'];
+Rotkin1 = [0 0 0]; % euler kin
+Rotkin2 = [0 0 0 0 0 0 0 0 0]; %strap down 
+Rotkin3_quat = [1 0 0 0]; %quat kin
+position = [6371800 0 0];% position
+unused_position = [6371800 0 0];%unused
+w = [0 0 0];%body rate
+v = [13 0 0];%velocity
 
+x = [Rotkin1 Rotkin2 Rotkin3_quat position unused_position w v];
 
+ix = [];
+iu = [];
+iy = [];
+u = [];
+y = [];
 
-[trimmed] = trim("ame532Project1_onedrive",ic,0,0);
+[x,u,y] = trim('ame532Project1_onedrive',x,u,y,ix,iu,iy)
